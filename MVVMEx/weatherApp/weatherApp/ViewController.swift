e5d142dc98f5b94caecef545d1a131efebd81e65
Jiseok97 <wltjr1426@naver.com>
2022-01-07 12:56:06 +0900

    MVVMEx Setup

---
+++ MVVMEx/weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by 이지석 on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
+
