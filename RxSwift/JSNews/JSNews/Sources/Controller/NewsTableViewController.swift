7755b65c03361e2b7550550393b17ec46f74f8b5
Jiseok97 <wltjr1426@naver.com>
2022-04-21 13:13:54 +0900

    [Set] JSNews 초기 셋팅

---
+++ RxSwift/JSNews/JSNews/Sources/Controller/NewsTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NewsTableViewController.swift
+//  JSNews
+//
+//  Created by 이지석 on 2022/04/21.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifycycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    // MARK: - Functions
+    
+    
+}
