c33c8013f8870398a7b9c1b3750d7693496c366b
Jiseok97 <wltjr1426@naver.com>
2022-05-14 15:36:26 +0900

    [Style] ImageView, Button 추가

---
+++ RxSwift/CameraFilter_snapkit/CameraFilter_snapkit/Sources/PhotosCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewController.swift
+//  CameraFilter_snapkit
+//
+//  Created by 이지석 on 2022/05/14.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+}
---
+++ RxSwift/CameraFilter_snapkit/CameraFilter_snapkit/Sources/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  CameraFilter_snapkit
+//
+//  Created by 이지석 on 2022/05/14.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    private lazy var photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = .black
+        return iv
+    }()
+    
+    private lazy var applyButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.tintColor = .link
+        button.setTitle("Apply", for: .normal)
+        button.addTarget(self, action: #selector(handleApply), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+        
+        configureUI()
+        configureNavigationBar()
+    }
+    
+    // MARK: - Selector
+    
+    @objc func handleAdd() {
+        let photosController = PhotosCollectionViewController()
+        navigationController?.pushViewController(photosController, animated: true)
+    }
+    
+    @objc func handleApply() {
+        print("DEBUG: Apply button tapped")
+    }
+
+    
+    // MARK: - Functions
+    
+    private func configureUI() {
+        
+        self.view.addSubview(photoImageView)
+        photoImageView.snp.makeConstraints { make in
+            make.top.equalTo(self.view).offset(16)
+            make.leading.equalTo(self.view).offset(24)
+            make.trailing.equalTo(self.view).offset(-24)
+            make.height.equalToSuperview().multipliedBy(0.8)
+        }
+        
+        self.view.addSubview(applyButton)
+        applyButton.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 120, height: 42))
+            make.top.equalTo(photoImageView.snp.bottom).offset(24)
+            make.centerX.equalToSuperview()
+        }
+        
+    }
+    
+    private func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+//        appearance.backgroundColor = .black
+//        appearance.titleTextAttributes = [.foregroundColor: UIColor.link]
+        
+//        navigationItem.title = "Edit Profile"
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.tintColor = .link
+        
+
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAdd))
+    }
+
+}
+
