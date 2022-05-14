d637bb2970fa16aea99b964aa37a8154c25f60b3
Jiseok97 <wltjr1426@naver.com>
2022-05-14 15:07:22 +0900

    [Style] 네비게이션 임베드

--- RxSwift/CameraFilter_snapkit/CameraFilter_snapkit/ViewController.swift
+++ RxSwift/CameraFilter_snapkit/CameraFilter_snapkit/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .yellow
         
+        configureNavigationBar()
+    }
+    
+    // MARK: - Selector
+    
+    @objc func handleAdd() {
+        print("Tapped nav")
     }
 
+    
+    // MARK: - Functions
+    
+    private func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+//        appearance.backgroundColor = .black
+//        appearance.titleTextAttributes = [.foregroundColor: UIColor.link]
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.tintColor = .link
+        
+//        navigationItem.title = "Edit Profile"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAdd))
+    }
 
 }
 
